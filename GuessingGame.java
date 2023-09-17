a4786cba82e0722d30f251ebc28cc5967bdb18d2
MomohAugustineDuwai <Momoh22000@student.hamk.fi>
2023-09-17 14:42:48 +0300

    Define the print method

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,28 +1,33 @@
 import java.util.Scanner;
 
 public class GuessingGame {
+    //Define the print method 
+    public static void print(String message ) {
+        System.out.println(message);
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String targetName = "Emma";
         String guess;
         int attempts = 0;
 
-        System.out.println("Guess my name (type stop to exit)");
+        print("Guess my name (type stop to exit)");
 
         while (true) {
             guess = scanner.nextLine();
             attempts++;
 
             if (guess.equalsIgnoreCase(targetName)) {
-                System.out.println("Congratulations!");
-                System.out.println("You guessed " + attempts  + " times.");
+                print("Congratulations!");
+
+                print("You guessed " + attempts  + " times.");
                 break;
             } else if (guess.equalsIgnoreCase("stop")) {
                 //System.out.println("Game over. You stopped guessing.");
-                System.out.println("You guessed " + (attempts -1) + " times.");
+               print("You guessed " + (attempts -1) + " times.");
                 break;
             } else {
-                System.out.println("Guess my name (type stop to exit)");
+                print("Guess my name (type stop to exit)");
             }
         }
 
